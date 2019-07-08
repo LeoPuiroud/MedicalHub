b1af87caaa4457b994b5859ed1eb6d682285da7d
LeoPuiroud <leo.puiroud@gmail.com>
2019-07-08 17:28:29 +0200

    queries dans repo

--- src/main/java/projet/repository/PatientRepository.java
+++ src/main/java/projet/repository/PatientRepository.java
@@ -1,9 +1,16 @@
 package projet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import projet.metier.Patient;
+import projet.metier.User;
 
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
+	@Query("select distinct pat from User pat left join fetch pat.prdv prdv left join fetch where pat.username=:username")
+	Optional<User> FindByIdWithUsernames(String username);
+	
 }
--- src/main/java/projet/repository/PraticienRepository.java
+++ src/main/java/projet/repository/PraticienRepository.java
@@ -1,9 +1,16 @@
 package projet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import projet.metier.Praticien;
+import projet.metier.User;
 
 public interface PraticienRepository extends JpaRepository<Praticien, Integer>{
 
+	@Query("select distinct prat from User prat left join fetch prat.patient pat left join fetch prat.specs specs left join fetch prat.drdv drdv where prat.username=:username")
+	Optional<User> FindByIdWithUsernames(String username);
+	
 }
