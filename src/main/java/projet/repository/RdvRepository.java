b3ee3d095bbf484f2e9d566713bea35dae59c824
LeoPuiroud <leo.puiroud@gmail.com>
2019-07-08 16:41:46 +0200

    modif leo

--- src/main/java/projet/repository/RdvRepository.java
+++ src/main/java/projet/repository/RdvRepository.java
@@ -1,9 +1,16 @@
 package projet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import projet.metier.Rdv;
+import projet.metier.User;
 
 public interface RdvRepository extends JpaRepository<Rdv, Integer> {
 
+	@Query("select distinct r from Rdv r left join fetch r.praticien left join fetch r.patient left join fetch r.motif where r.id=:id")
+	Optional<User> FindByIdWithUsernames(Integer id);
+	
 }
--- src/main/java/projet/repository/UserRepository.java
+++ src/main/java/projet/repository/UserRepository.java
@@ -1,10 +1,15 @@
 package projet.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import projet.metier.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 	
+	@Query("select distinct u from User u left join fetch u.roles where u.username=:username")
+	Optional<User> FindByIdWithUsernames(String username);
 
 }
