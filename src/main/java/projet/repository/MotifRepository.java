9441f460f6c088f322a790f7580ec57b7cea03d2
LeoPuiroud <leo.puiroud@gmail.com>
2019-07-08 15:21:21 +0200

    ajout repository

---
+++ src/main/java/projet/repository/MotifRepository.java
@@ -0,0 +1,9 @@
+package projet.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import projet.metier.Motif;
+
+public interface MotifRepository extends JpaRepository<Motif,Integer>{
+
+}
